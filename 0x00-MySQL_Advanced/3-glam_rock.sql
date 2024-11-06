e2f66d58ec75e46f30b34208d27c5d1837d42168
Peter Sylvester Lansana <Lansanapeters@gmail.com>
2024-11-06 23:12:05 +0000

    Old school band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,8 +1,6 @@
--- Write a SQL script that lists all bands with Glam rock as their main style,
--- ranked by their longevity
-SELECT name AS band_name,
-       IFNULL(split, 2022)
+-- script that lists all bands with Glam rock as their main style, ranked by their longevity
+
+SELECT
+    band_name, (IFNULL(split, 2022) - formed) AS lifespan
 FROM metal_bands
-WHERE FIND_IN_SET("Glam rock", style)
-value
-ORDER BY lifespan DESC;
+WHERE style LIKE '%Glam rock%';
